518e00cc355753827724a139cd819fd7743b6fb4
deg <david.deguara@mcast.edu.mt>
2022-10-12 13:03:15 +0200

    Image loaded successfully into Unity! *Celebrate here*

--- Assets/Scripts/FirebaseStorgeController.cs
+++ Assets/Scripts/FirebaseStorgeController.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
+
 using Firebase.Storage;
 using Firebase.Extensions;
+using UnityEngine.UI;
+
 public class FirebaseStorgeController : MonoBehaviour
 {
     
     private FirebaseStorage _instance;
-
+    
     private void Awake()
     {
         _instance = FirebaseStorage.DefaultInstance;
@@ -30,9 +33,13 @@ public class FirebaseStorgeController : MonoBehaviour
                 // Uh-oh, an error occurred!
             }
             else {
-                byte[] fileContents = task.Result;
-                Debug.Log(fileContents.Length);
-                Debug.Log("Finished downloading!");
+                Debug.Log($"{storageRef.Name} finished downloading!");
+                //Load the image into Unity
+                Texture2D imageTex = new Texture2D(1, 1);
+                imageTex.LoadImage(task.Result);
+                Sprite downloadedImage = Sprite.Create(imageTex,new Rect(0,0,imageTex.width,
+                    imageTex.height),new Vector2(imageTex.width/2,imageTex.height/2));
+                GameObject.Find("Downloaded_Image").GetComponent<Image>().sprite = downloadedImage;
             }
         });
         
